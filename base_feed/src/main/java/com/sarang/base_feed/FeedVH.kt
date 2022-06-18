c68d698de94bd13ef019af724740d933044607f9
Sarang Yang <sarang628@naver.com>
2022-06-18 13:05:29 +0900

    잿팩 컴포즈 임시 적용 Temporarily apply Jetpack Compose

--- base_feed/src/main/java/com/sarang/base_feed/FeedVH.kt
+++ base_feed/src/main/java/com/sarang/base_feed/FeedVH.kt
@@ -1,14 +1,52 @@
 package com.sarang.base_feed
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.get
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.LifecycleOwner
 import androidx.recyclerview.widget.RecyclerView
+import com.example.torang_core.data.model.Feed
 import com.google.android.material.tabs.TabLayoutMediator
 import com.sarang.base_feed.databinding.ItemTimeLineBinding
-import com.example.torang_core.data.model.Feed
+
+
+/** Compose test */
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg: Message) {
+    // Add padding around our message
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.b3s),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+        )
+
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(text = msg.author)
+            // Add a vertical space between the author and message texts
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
+    }
+}
 
 /**
  * [ItemTimeLineBinding]
@@ -24,9 +62,11 @@ class FeedVH(
         itemTimeLineBinding.lifecycleOwner = lifeCycleOwner
         val timeLinePictureRvAdt = FeedPictureVpAdt(viewModel)
         itemTimeLineBinding.viewpager.adapter = timeLinePictureRvAdt
+
+        itemTimeLineBinding.greeting.setContent{
+            //MessageCard(Message("Android", "Jetpack Compose"))
+        }
     }
-
-
 
     fun setFeed(feed: Feed) {
         itemTimeLineBinding.feed = feed
@@ -49,8 +89,6 @@ class FeedVH(
         viewModel.isFavorite(feed.review_id).observe(lifeCycleOwner) {
             itemTimeLineBinding.favorite = it
         }
-
-
     }
 
     companion object {
@@ -66,4 +104,10 @@ class FeedVH(
             )
         }
     }
+}
+
+@Preview
+@Composable
+fun PreViewMessageCard() {
+    MessageCard(Message("Android1", "Jetpack Compose"))
 }
