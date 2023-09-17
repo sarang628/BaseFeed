022f37a329fc9147db18711fdf0bc3de5cea088c
SRYANG <sarang628@naver.com>
2023-09-17 18:51:52 +0800

    update

--- library/src/main/java/com/sarang/base_feed/uistate/FeedTopUIState.kt
+++ library/src/main/java/com/sarang/base_feed/uistate/FeedTopUIState.kt
@@ -5,6 +5,7 @@ import com.sarang.base_feed.data.Feed
 /*피드 상단 UIState*/
 data class FeedTopUIState(
     val reviewId: Int? = 0,
+    val userId : Int? = 0,
     val name: String? = null,
     val restaurantName: String? = null,
     val rating: Float? = null,
@@ -14,6 +15,7 @@ data class FeedTopUIState(
 fun Feed.FeedTopUiState(): FeedTopUIState {
     return FeedTopUIState(
         reviewId = this.reviewId,
+        userId = this.userId,
         name = this.name,
         restaurantName = this.restaurantName,
         rating = this.rating,
