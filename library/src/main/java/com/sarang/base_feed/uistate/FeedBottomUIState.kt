9e58d055c7b76e89c945ccb2bfbc7c81279a4480
SRYANG <sarang628@naver.com>
2023-09-22 10:08:47 +0800

    update

--- library/src/main/java/com/sarang/base_feed/uistate/FeedBottomUIState.kt
+++ library/src/main/java/com/sarang/base_feed/uistate/FeedBottomUIState.kt
@@ -5,17 +5,17 @@ package com.sarang.base_feed.uistate
 /*피드 하단 UIState*/
 data class FeedBottomUIState(
     val reviewId: Int,
-    val likeAmount: Int? = 0,
-    val commentAmount: Int? = 0,
-    val author: String? = "",
-    val author1: String? = "",
-    val author2: String? = "",
-    val comment: String? = "",
-    val comment1: String? = "",
-    val comment2: String? = "",
-    val isLike: Boolean? = false,
-    val isFavorite: Boolean? = false,
-    val visibleLike: Boolean? = false,
-    val visibleComment: Boolean? = false,
-    val contents: String? = ""
+    val likeAmount: Int,
+    val commentAmount: Int,
+    val author: String,
+    val author1: String,
+    val author2: String,
+    val comment: String,
+    val comment1: String,
+    val comment2: String,
+    val isLike: Boolean,
+    val isFavorite: Boolean,
+    val visibleLike: Boolean,
+    val visibleComment: Boolean,
+    val contents: String
 )
--- library/src/main/java/com/sarang/base_feed/uistate/FeedUiState.kt
+++ library/src/main/java/com/sarang/base_feed/uistate/FeedUiState.kt
@@ -2,11 +2,8 @@ package com.sarang.base_feed.uistate
 
 /*피드 UIState*/
 data class FeedUiState(
-    val reviewId: Int? = 0,
+    val reviewId: Int,
     val itemFeedTopUiState: FeedTopUIState,
     val itemFeedBottomUiState: FeedBottomUIState,
-    val reviewImages: List<String>? = ArrayList(),
-    val visibleReviewImage: Boolean = false,
-    val imageClickListener: ((Int) -> Unit)? = null,
-
+    val reviewImages: List<String> = ArrayList(),
     )
