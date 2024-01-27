3d796f13b22a5326cc5418ac082aabc91f004c8c
SRYANG <sarang628@naver.com>
2024-01-27 17:09:53 +0900

    update dark theme like button

--- library/src/main/java/com/sarang/torang/compose/feed/internal/components/FavoriteImage.kt
+++ library/src/main/java/com/sarang/torang/compose/feed/internal/components/FavoriteImage.kt
@@ -3,16 +3,18 @@ package com.sarang.torang.compose.feed.internal.components
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.material.icons.materialPath
 import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
-import com.sarang.torang.R
+import androidx.compose.ui.unit.dp
 import com.sarang.torang.compose.feed.internal.util.nonEffectclickable
 
 @Composable
@@ -20,8 +22,8 @@ fun FavoriteImage(
     modifier: Modifier, isFavorite: Boolean, onFavorite: () -> Unit, size: Dp,
     padding: Dp
 ) {
-    Icon(imageVector = if (isFavorite) Icons.Outlined.Star
-    else Icons.Filled.Star,
+    Icon(
+        imageVector = if (isFavorite) Icons.Default.Star else Icons.Outlined.Star,
         contentDescription = "favorite",
         modifier = modifier
             .size(size)
@@ -29,4 +31,16 @@ fun FavoriteImage(
             .nonEffectclickable {
                 onFavorite.invoke()
             })
+}
+
+@Preview
+@Composable
+fun PreviewFavoriteImage() {
+    FavoriteImage(
+        modifier = Modifier,
+        isFavorite = false,
+        onFavorite = { /*TODO*/ },
+        size = 50.dp,
+        padding = 5.dp
+    )
 }
--- library/src/main/java/com/sarang/torang/compose/feed/internal/components/LikeImage.kt
+++ library/src/main/java/com/sarang/torang/compose/feed/internal/components/LikeImage.kt
@@ -4,8 +4,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.outlined.Favorite
-import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
