88104d4dbc3dc9620ac1c4b7cd0583844f16e0f3
sryang <sarang628@gmail.com>
2024-07-27 22:40:22 +0800

    change ExpandableText

--- app/src/main/java/com/sarang/torang/ThemeTest.kt
+++ app/src/main/java/com/sarang/torang/ThemeTest.kt
@@ -1,12 +1,9 @@
 package com.sarang.torang
 
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.google.samples.apps.sunflower.ui.TorangTheme
 import com.sarang.torang.compose.feed.PreViewFeed
-import com.sarang.torang.compose.feed.internal.components.PreviewExpandableText
 import com.sarang.torang.ui.theme.ThemePreviews
 
 
@@ -25,7 +22,7 @@ fun PreviewFeed() {
 fun test1() {
     TorangTheme {
         Surface {
-            PreviewExpandableText()
+
         }
     }
 }
