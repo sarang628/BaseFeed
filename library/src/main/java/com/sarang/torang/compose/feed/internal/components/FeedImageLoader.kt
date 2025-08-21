181f3893ca133581e93a11236952242bd8e98155
sryang <sarang628@gmail.com>
2025-08-21 18:39:34 +0800

    refactor : add sample image

--- library/src/main/java/com/sarang/torang/compose/feed/internal/components/FeedImageLoader.kt
+++ library/src/main/java/com/sarang/torang/compose/feed/internal/components/FeedImageLoader.kt
@@ -1,14 +1,24 @@
 package com.sarang.torang.compose.feed.internal.components
 
 import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
+import com.sarang.torang.R
 
 typealias FeedImageLoader = @Composable (Modifier, String, Dp?, Dp?, ContentScale?, Dp?) -> Unit
 
 val LocalFeedImageLoader = compositionLocalOf<FeedImageLoader> {
-    @Composable { _, _, _, _, _, _ -> Log.w("__LocalFeedImageLoader", "imageLoadCompose doesn't set") }
+    @Composable { modifier, _, _, _, _, _ ->
+        Log.w("__LocalFeedImageLoader", "imageLoadCompose doesn't set")
+        Image(modifier = modifier, painter = painterResource(R.drawable.sample), contentDescription = "", contentScale = ContentScale.Crop)
+    }
 }
